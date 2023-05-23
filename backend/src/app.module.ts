1f9a527b9b50ccf92bbee08772db5779f254b487
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-05-23 18:06:48 -0300

    🔨 update app module's dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [NoteModule],
 })
 export class AppModule {}
