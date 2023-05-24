a22d3c5c364b2bbdd1cf0a33f10e7f76dca05d3d
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-05-23 23:18:59 -0300

    🔨 note's DTO created

---
+++ backend/src/note/dto/create-note.dto.ts
@@ -0,0 +1,15 @@
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateNoteDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsArray()
+  @IsOptional()
+  categories?: number[];
+}
---
+++ backend/src/note/dto/index.ts
@@ -0,0 +1,3 @@
+export * from './active-modifier-note.dto';
+export * from './create-note.dto';
+export * from './edit-note.dto';
