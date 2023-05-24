4e927f6cdd1ed6b39b0b980adcfb25c355f6784b
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-05-23 23:21:44 -0300

    🐛 Prisma & Config modules imported at App

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { NoteModule } from './note/note.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NoteModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    NoteModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
