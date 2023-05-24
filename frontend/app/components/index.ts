93cc56191b82873fa269b5bae27104b3e4aa1ef0
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-05-24 15:38:28 -0300

    🔨 component's barrel created

---
+++ frontend/app/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from './Header/Header';
+export { default as NavLinks } from './NavLinks/NavLinks';
+export * from './NavLinks/NavLinks.types';
+export { default as Note } from './NoteCard/NoteCard';
+export * from './NoteCard/NoteCard.types';
+export * from './index';
