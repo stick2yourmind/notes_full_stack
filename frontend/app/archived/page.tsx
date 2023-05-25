639be94afc6a3fc09477fcc6d8ae8ca88a2ef8ce
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-05-24 23:37:34 -0300

    🔨 active & archived notes page created

--- frontend/app/archived/page.tsx
+++ frontend/app/archived/page.tsx
@@ -1,23 +1,33 @@
-import { Note } from "../components";
-import { notesMock } from "../data/notesMock";
+"use client"
 
-export default function Archived() {
+import { NoteCard, Note } from "../components";
+import { getArchivedNotes } from "../services";
+
+export default async function Archived() {
+  const notesData = await getArchivedNotes()
+  const archivedNote = notesData
   return (
     <main className="flex flex-col w-full justify-start max-w-5xl bg-stone-500/30 p-2 rounded-md">
       <h2 className="text-lg text-pink-600 font-bold">
         My archived notes
       </h2>
       <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        {
-          notesMock.map(note => 
-          <Note 
-            title={note.title}
-            description={note.description}
-            updatedAt={note.updatedAt}
-            key={note.id}
-          />)
+        { archivedNote === null ? "Could not fetch data" : 
+          !archivedNote.length 
+              ? "There are no archived notes" 
+              : archivedNote.map(note => 
+                <NoteCard 
+                  title={note.title}
+                  description={note.description}
+                  updatedAt={note.updatedAt}
+                  archived={note.archived}
+                  id={note.id}
+                  key={note.id}
+                />)
         }
       </section>
     </main>
   )
 }
+
+export const revalidate = 1; 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,31 @@
-import { Note } from "./components";
-import { notesMock } from "./data/notesMock";
+"use client"
 
-export default function Home() {
+import { NoteCard } from "./components";
+import { getActiveNotes } from "./services";
+
+export default async function Home() {
+  const notesData = getActiveNotes()
+  const activeNote = await notesData
+  
   return (
     <main className="flex flex-col w-full justify-start max-w-5xl bg-stone-500/30 p-2 rounded-md">
       <h2 className="text-lg text-pink-600 font-bold">
         My active notes
       </h2>
       <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        {
-          notesMock.map(note => 
-          <Note 
-            title={note.title}
-            description={note.description}
-            updatedAt={note.updatedAt}
-            key={note.id}
-          />)
-        }
+      { activeNote === null ? "Could not fetch data" : 
+          !activeNote.length 
+              ? "There are no active notes" 
+              : activeNote.map(note => 
+                <NoteCard 
+                  title={note.title}
+                  description={note.description}
+                  updatedAt={note.updatedAt}
+                  archived={note.archived}
+                  id={note.id}
+                  key={note.id}
+                />)
+          }
       </section>
     </main>
   )
