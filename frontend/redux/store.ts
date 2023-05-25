8f44f9ea9dabb554a8571f1729e84cacfc313be3
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-05-25 05:19:34 -0300

    🔨 create edit modal slice

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,13 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import deleteModalReducer from "./features/deleteModalSlice";
-import toastReducer from "./features/toastSlice";
-import noteReducer from "./features/noteSlice";
+import { createEditModalSlice } from './features/createEditModalSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import deleteModalReducer from './features/deleteModalSlice';
+import toastReducer from './features/toastSlice';
+import noteReducer from './features/noteSlice';
+import createEditModalReducer from './features/createEditModalSlice';
 
 export const store = configureStore({
   reducer: {
     deleteModalReducer,
     toastReducer,
-    noteReducer
+    noteReducer,
+    createEditModalReducer,
   },
 });
 
