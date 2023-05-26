347980bd6fb1912788437a8f53c1fd15731f22e2
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-05-25 23:39:10 -0300

    ♻️ home page modified to router pages version

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,40 @@
-'use client';
-/* eslint-disable prettier/prettier */
 /* eslint-disable @next/next/no-img-element */
-
-import { useDispatch } from 'react-redux';
-import { NoteCard } from './components';
-import { getActiveNotes } from './services';
+import { NoteCard } from '@/components';
 import { CreateEditModalType, enableCreateEditModal } from '@/redux/features/createEditModalSlice';
+import { setNotesState } from '@/redux/features/noteSlice';
+import { RootState } from '@/redux/store';
+import { getActiveNotes } from '@/services';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default async function Home() {
-  const dispatch = useDispatch()
-  const notesData = getActiveNotes();
-  const activeNote = await notesData;
+
+export default function Home() {
+  const dispatch = useDispatch();
+  const noteState = useSelector((state: RootState) => state.noteReducer.notes);
+  const activeNote = noteState.filter((note) => note.archived === false);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const fetchData = async () => {
+      const resData = getActiveNotes(abortController);
+      const data =  await resData;
+      return data;
+    };
+    fetchData()
+      .then(notes => dispatch(setNotesState(notes || [])));
+  
+    return () => {
+      abortController.abort();
+    };
+  }, []);
 
   const createNote = () => {
-    dispatch(enableCreateEditModal({ type: CreateEditModalType.CREATE, currentNote: null }))
-  }
-
+    dispatch(enableCreateEditModal({ type: CreateEditModalType.CREATE, currentNote: null }));
+  };
+  
   return (
     <main className="flex flex-col w-full justify-start max-w-5xl bg-stone-500/30 p-2 rounded-md">
+
       <h2 className="text-lg text-pink-600 font-bold">My active notes</h2>
       <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
 
